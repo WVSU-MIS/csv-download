aafe9e1691b062503f6733eaab43bbbfc2f8ce32
unknown <louiecervantes@wvsu.edu.ph>
2023-05-15 16:04:16 +0800

    save changes

---
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -0,0 +1,45 @@
+#Write a simple app that reads the user input and display the output
+import streamlit as st
+import pandas as pd
+import altair as alt
+
+
+openai.api_key = st.secrets["API_key"]
+
+
+# Define the Streamlit app
+def app():
+
+def save_and_download_csv(dataframe):
+    # Save the dataframe as a CSV file
+    dataframe.to_csv('output.csv', index=False)
+
+    # Generate a download link for the CSV file
+    with open('output.csv', 'rb') as file:
+        csv_data = file.read()
+    st.download_button(label='Download CSV', data=csv_data, \
+                       file_name='output.csv', mime='text/csv')
+
+# Streamlit app
+def main():
+    st.title("CSV File Download")
+    st.write("Generate a CSV file and download it.")
+
+    # Create a sample dataframe
+    data = {'Name': ['John', 'Jane', 'Mike'],
+            'Age': [25, 30, 35]}
+    df = pd.DataFrame(data)
+
+    # Display the dataframe
+    st.header("Dataframe")
+    st.write(df)
+
+    # Save and download the CSV file
+    save_and_download_csv(df)
+
+if __name__ == '__main__':
+    main()
+
+# Run the app
+if __name__ == "__main__":
+    app()
